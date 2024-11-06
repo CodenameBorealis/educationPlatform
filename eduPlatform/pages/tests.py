0f6b1c13fba5abe84673dde41072aaa4996b9e52
finista <95535357+finista@users.noreply.github.com>
2024-11-06 22:50:01 +0300

    Removed pychache
    Removed pycache and added it into gitignore to prevent it from appearing in commits again

--- eduPlatform/pages/tests.py
+++ eduPlatform/pages/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ExpTestCase(TestCase):
+    def setUp(self) -> None:
+        self.testCase = False
+    
+    def test_case(self):
+        assert(self.testCase)
