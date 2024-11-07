7045b2b36b92eb9a41aee0e34093974edeb14e1a
finista <95535357+finista@users.noreply.github.com>
2024-11-07 03:36:30 +0300

    Added test cases
    Added test cases for API and pages to ensure successful deployment of the product.

--- eduPlatform/api/tests.py
+++ eduPlatform/api/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from . import models
 
 # Create your tests here.
+class TestApi(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_api1(self, *args, **kwargs):
+        objects = models.TestInformation.objects
+        for i in range(50):
+            objects.create(name=f"Name{i}", description=f"Description{i}")
+
+        request = self.client.get("/api/testing_api/")
+
+        self.assertIsNotNone(request)
+        self.assertEqual(request.status_code, 200)
+        self.assertEqual(len(request.json()), 50)
--- eduPlatform/pages/tests.py
+++ eduPlatform/pages/tests.py
@@ -1,9 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
-class ExpTestCase(TestCase):
-    def setUp(self) -> None:
-        self.testCase = False
+class TestHomePage(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        User.objects.create_user(username='testuser', password='12345')
+
+        self.client = Client()
+        logged_in = self.client.login(username='testuser', password='12345')
+
+        self.assertNotEqual(logged_in, False)
+
+    def test_homepage_url_exists_at_desired_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
     
-    def test_case(self):
-        assert(self.testCase)
+    def test_homepage_corresponds_to_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_homepage_accessible_with_no_login(self):
+        self.client.logout()
+        
+        response = self.client.get("/")
+        self.assertTemplateNotUsed(response, "home.html")
