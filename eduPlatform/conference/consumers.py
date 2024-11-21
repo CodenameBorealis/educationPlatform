de593cf805c92091f9d3fee9d3a577fab0131414
finista <95535357+finista@users.noreply.github.com>
2024-11-22 00:49:15 +0300

    Signaling changes and added tests
    Changed signaling to use private groups for direct messages using 'to' argument and added tests for SignalingConsumer

--- eduPlatform/conference/consumers.py
+++ eduPlatform/conference/consumers.py
@@ -1,24 +1,28 @@
 import json
-from channels.db import sync_to_async, database_sync_to_async as dsa
+from channels.db import database_sync_to_async as dsa
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class SignalingConsumer(AsyncWebsocketConsumer):
     def conferenceExists(self, conference_token):
         return self.Conference.objects.filter(token=conference_token).exists()
-        
+    
     async def connect(self):
         from .models import Conference
+        from django.contrib.auth import get_user_model
+        
         self.Conference = Conference
+        self.User = get_user_model()
         
         self.room_token = self.scope['url_route']['kwargs']['token']
-        self.room_group_name = f'signalling_{self.room_token}'
-    
+        self.room_group_name = f'signaling_{self.room_token}'
+        
         if not self.scope['user'].is_authenticated:
             await self.close(code=4001)
             return
-    
+        
+        self.private_group_name = f'private_user{self.scope.get("user").id}'
+        
         conferenceExistsAsync = dsa(self.conferenceExists)
-
         if not await conferenceExistsAsync(self.room_token):
             await self.close(code=4002)
             return
@@ -27,6 +31,12 @@ class SignalingConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+        await self.channel_layer.group_add(
+            self.private_group_name,
+            self.channel_name
+        )
+        
         await self.accept()
     
     async def disconnect(self, *args, **kwargs):
@@ -34,11 +44,16 @@ class SignalingConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+        
+        await self.channel_layer.group_discard(
+            self.private_group_name,
+            self.channel_name
+        )
     
     async def receive(self, text_data):
         data = json.loads(text_data)
         
-        if data.get("type") == "join":        
+        if data.get("type") == "join":      
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
@@ -49,14 +64,20 @@ class SignalingConsumer(AsyncWebsocketConsumer):
                     }
                 }
             )
-        else:
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
+            
+            return
+        
+        room_group = (
+            self.room_group_name
+            if not data.get("to")
+            else f"private_user{data.get('to')}"
+        )
+        
+        await self.channel_layer.group_send(
+            room_group, {
                     'type': 'signaling_message',
                     'message': data
-                }
-            )
+                })
     
     async def signaling_message(self, event):
         message = event.get("message")
--- eduPlatform/conference/tests.py
+++ eduPlatform/conference/tests.py
@@ -1,3 +1,97 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from channels.testing import WebsocketCommunicator
+from channels.auth import AuthMiddlewareStack
+from channels.db import database_sync_to_async as dsa
+from channels.layers import get_channel_layer
 
-# Create your tests here.
+from .consumers import SignalingConsumer
+from eduPlatform.asgi import application
+from .models import Conference
+
+class SignalingConsumerTest(TestCase):
+    async def asyncSetUp(self):
+        self.User = get_user_model()
+        self.user = await dsa(self.User.objects.create_testuser)()
+        self.user.set_password("test")
+        await dsa(self.user.save)()
+        
+        self.conference = await dsa(Conference.objects.create)(host=self.user)
+        await dsa(self.conference.save)()
+        
+        self.communicator = WebsocketCommunicator(
+            AuthMiddlewareStack(application), f"/ws/signaling/{self.conference.token}/"
+        )
+        self.communicator.scope["user"] = self.user
+        
+        connected, _ = await self.communicator.connect()
+        self.assertTrue(connected, "Failed to connect to websocket")
+        
+        self.assertEqual(self.communicator.scope["user"].username, "test", "Invalid scope user username")
+    
+    async def tearDown(self):
+        await self.communicator.disconnect()
+    
+    async def test_unauthorized(self):
+        await self.asyncSetUp()
+        
+        communicator = WebsocketCommunicator(
+            AuthMiddlewareStack(application), f"/ws/signaling/{self.conference.token}/"
+        )
+        
+        connected, _ = await communicator.connect() 
+        
+        self.assertFalse(connected, "Connected without proper authentication.")
+    
+        await self.tearDown()
+    
+    async def test_data_signaling(self):
+        await self.asyncSetUp()
+        
+        channel_layer = get_channel_layer()
+        group = f"signaling_{self.conference.token}"
+
+        await channel_layer.group_send(
+            group, {
+                "type": "signaling_message",
+                "message": {
+                    "type": "join",
+                    "userId": str(self.user.id),
+                }
+            }
+        )
+        
+        response = await self.communicator.receive_json_from(timeout=5)
+        
+        self.assertEqual(response, {"type": "join", "userId": str(self.user.id)}, "Got an invalid JSON response")
+    
+        await self.tearDown()
+    
+    async def test_private_data_signaling(self):
+        await self.asyncSetUp()
+        
+        channel_layer = get_channel_layer()
+        group = f"private_user{self.user.id}"
+        
+        data = {
+            "type": "signaling_message",
+            "message": {
+                "type": "test_signal",
+                "to": str(self.user.id),
+                "from": str(self.user.id),
+            }
+        }
+        
+        await channel_layer.group_send(
+            group, data
+        )
+        
+        response = await self.communicator.receive_json_from(timeout=5)
+        
+        self.assertEqual(response, {
+            "type": "test_signal",
+            "to": str(self.user.id),
+            "from": str(self.user.id),
+        }, "Got invalid JSON response.")
+        
+        await self.tearDown()
