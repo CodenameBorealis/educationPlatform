c00c2d3bef7c25a7918d088aaa8783cdb83d98bc
finista <95535357+finista@users.noreply.github.com>
2024-11-28 00:49:48 +0300

    Added screensharing feature, partial code refactor and I added comments
    Refactored the code in terms of formatting, added signaling for screenshare events and isolated signal interceptions for global-message, start-screenshare and etc. Added a lot of comments for whoever is gonna be reading this in the future.

--- eduPlatform/conference/consumers.py
+++ eduPlatform/conference/consumers.py
@@ -5,115 +5,172 @@ from channels.db import database_sync_to_async as dsa
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.cache import cache
 
+
 class SignalingConsumer(AsyncWebsocketConsumer):
     def conferenceExists(self, conference_token):
         return self.Conference.objects.filter(token=conference_token).exists()
-    
+
+    def getConferenceHostId(self, conference_token):
+        return self.Conference.objects.get(token=conference_token).host.id
+
+    # Screen share state variables
+    isScreenSharing = False
+    screenShareUserID = -1
+
     async def connect(self):
+        if not self.scope[
+            "user"
+        ].is_authenticated:  # Restrict access if user is not authenticated
+            await self.close(code=4001)
+            return
+
+        # Load main libraries here because if you do that outside it's gonna error out and I have no clue on how to fix it
         from .models import Conference
         from django.contrib.auth import get_user_model
-        
+
         self.Conference = Conference
         self.User = get_user_model()
-        
-        self.room_token = self.scope['url_route']['kwargs']['token']
-        self.room_group_name = f'signaling_{self.room_token}'
-        
+
+        # Store information about the room the user is currently in
+        self.room_token = self.scope["url_route"]["kwargs"]["token"]
+        self.room_group_name = f"signaling_{self.room_token}"
+
+        self.private_group_name = f'private_user{self.scope.get("user").id}'
+        self.hostId = await dsa(self.getConferenceHostId)(self.room_token)
+
+        # Check if the conference actually exists, if not, disconnect user
+        if not await dsa(self.conferenceExists)(self.room_token):
+            await self.close(code=4002)
+            return
+
+        # Save the cache key for message history and if the cache doesn't exist, start a new empty cache
         self.message_history_cache_key = f"conference_message_cache_{self.room_token}"
         if not cache.get(self.message_history_cache_key):
             cache.set(self.message_history_cache_key, [], timeout=86400)
-        
-        if not self.scope['user'].is_authenticated:
-            await self.close(code=4001)
-            return
-        
-        self.private_group_name = f'private_user{self.scope.get("user").id}'
-        
-        conferenceExistsAsync = dsa(self.conferenceExists)
-        if not await conferenceExistsAsync(self.room_token):
-            await self.close(code=4002)
-            return
-    
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
+
+        # If everything checks out, add user to the group channels and accept the websocket connection
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_add(self.private_group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, *args, **kwargs):
+        # An additional check if the user is disconnecting and was sharing their screen, call stop_screenshare and pass in a mockup data input
+        if self.scope["user"].id == self.screenShareUserID and self.isScreenSharing:
+            await self.stop_screenshare(
+                {"from": self.screenShareUserID, "type": "stop-screenshare"}
+            )
+
+        # Remove user from the existing group channels
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(
+            self.private_group_name, self.channel_name
         )
 
-        await self.channel_layer.group_add(
-            self.private_group_name,
-            self.channel_name
+    async def handle_global_chat_message(self, data):
+        # An async function used as an override for 'global-message' signal type
+
+        # Get current time and convert it to iso format for reginal sync
+        now = datetime.now()
+        timestamp = now.isoformat()
+
+        data["timestamp"] = timestamp
+
+        # Save the message to the message history cache
+        message_history = cache.get(self.message_history_cache_key, [])
+        message_history.append(
+            {
+                "user_id": data.get("from"),
+                "username": self.scope["user"].username,
+                "content": data.get("content"),
+                "timestamp": data.get("timestamp"),
+            }
         )
-        
-        await self.accept()
-    
-    async def disconnect(self, *args, **kwargs):
-        await self.channel_layer.group_discard(
+
+        cache.set(self.message_history_cache_key, message_history, timeout=86400)
+
+        # Send the message and it's data to other users
+        await self.channel_layer.group_send(
             self.room_group_name,
-            self.channel_name
+            {
+                "type": "signaling_message",
+                "username": self.scope.get("user").username,
+                "message": data,
+            },
         )
-        
-        await self.channel_layer.group_discard(
-            self.private_group_name,
-            self.channel_name
+
+    async def start_screenshare(self, data):
+        # Async function used as an override for 'start-screenshare' signal type
+
+        # Check user permissions (Don't worry about data.get("from"), it was set by the backend server in receive function)
+        if self.hostId != data.get("from"):
+            return
+
+        """
+        If the screen if being shared, ignore request to avoid duplicates
+        "data.get("to")" is used in case a user joins the conference while someone's already sharing their screen,
+        so the host can get the start-screenshare through just for that specific user
+        """
+        if self.isScreenSharing and not data.get("to"):
+            return
+
+        # Set the screenshare status to true and save the sharing user's ID
+        self.isScreenSharing = True
+        self.screenShareUserID = data.get("from")
+
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "signaling_message", "message": data}
         )
-    
+
+    async def stop_screenshare(self, data):
+        # Async function used as an override for 'stop-screenshare' signal type
+
+        # If no screenshare is taking place, just ignore the request
+        if not self.isScreenSharing:
+            return
+
+        # If the user who requested the screenshare to stop isn't the original user who started it, ignore the request.
+        if data.get("from") != self.screenShareUserID:
+            return
+
+        # Reset the screenshare attributes
+        self.isScreenSharing = False
+        self.screenShareUserID = -1
+
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "signaling_message", "message": data}
+        )
+
     async def receive(self, text_data):
+        # Async function used for handling data received from the clients
+
+        # Load the data and for security measures to not let impersonation take place, override 'from' field
         data = json.loads(text_data)
         data["from"] = self.scope["user"].id
-        
-        if data.get("type") == "join":      
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    'type': 'signaling_message',
-                    'message': {
-                        'type': 'new-participant',
-                        'from': data['from']
-                    }
-                }
-            )
-            
+
+        # If the 'type' of the signal is one of the rule-enforced methods or requires special handling, send it to a dedicated method
+        type = data.get("type")
+        if type == "global-message":
+            await self.handle_global_chat_message(data)
+            return
+        elif type == "start-screenshare":
+            await self.start_screenshare(data)
+            return
+        elif type == "stop-screenshare":
+            await self.stop_screenshare(data)
             return
 
-        if data.get("type") == "global-message":
-            now = datetime.now()
-            timestamp = now.isoformat()
-            
-            data["timestamp"] = timestamp
-            
-            message_history = cache.get(self.message_history_cache_key, [])
-            message_history.append({
-                'user_id': data.get("from"),
-                'username': self.scope["user"].username,
-                'content': data.get("content"),
-                'timestamp': data.get("timestamp")
-            })
-            
-            cache.set(self.message_history_cache_key, message_history, timeout=86400)
-            
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    "type": "signaling_message",
-                    "username": self.scope.get("user").username,
-                    "message": data
-                }
-            )
-            
-            return
-        
+        # If data doesn't have a field 'to', this is a global message and it will be sent to everyone, otherwise a private channel will be used.
         room_group = (
             self.room_group_name
             if not data.get("to")
             else f"private_user{data.get('to')}"
         )
-        
+
         await self.channel_layer.group_send(
-            room_group, {
-                    'type': 'signaling_message',
-                    'message': data
-                })
-    
+            room_group, {"type": "signaling_message", "message": data}
+        )
+
     async def signaling_message(self, event):
+        # Async function used for "json'ifying the data and sending it to the client"
         message = event.get("message")
         await self.send(text_data=json.dumps(message))
--- eduPlatform/conference/tests.py
+++ eduPlatform/conference/tests.py
@@ -10,136 +10,212 @@ from eduPlatform.asgi import application
 from .consumers import SignalingConsumer
 from .models import Conference
 
+
 class SignalingConsumerTest(TestCase):
     async def asyncSetUp(self):
         self.User = get_user_model()
         self.user = await dsa(self.User.objects.create_testuser)()
         self.user.set_password("test")
         await dsa(self.user.save)()
-        
+
         self.conference = await dsa(Conference.objects.create)(host=self.user)
         await dsa(self.conference.save)()
-        
+
         self.communicator = WebsocketCommunicator(
             AuthMiddlewareStack(application), f"/ws/signaling/{self.conference.token}/"
         )
         self.communicator.scope["user"] = self.user
-        
+
         connected, _ = await self.communicator.connect()
         self.assertTrue(connected, "Failed to connect to websocket")
-        
-        self.assertEqual(self.communicator.scope["user"].username, "test", "Invalid scope user username")
-    
+
+        self.assertEqual(
+            self.communicator.scope["user"].username,
+            "test",
+            "Invalid scope user username",
+        )
+
     async def tearDown(self):
         await self.communicator.disconnect()
-    
+
     async def test_unauthorized(self):
         await self.asyncSetUp()
-        
+
         communicator = WebsocketCommunicator(
             AuthMiddlewareStack(application), f"/ws/signaling/{self.conference.token}/"
         )
-        
-        connected, _ = await communicator.connect() 
-        
+
+        connected, _ = await communicator.connect()
+
         self.assertFalse(connected, "Connected without proper authentication.")
-    
+
         await self.tearDown()
-    
+
     async def test_data_signaling(self):
         await self.asyncSetUp()
-        
+
         channel_layer = get_channel_layer()
         group = f"signaling_{self.conference.token}"
 
         await channel_layer.group_send(
-            group, {
+            group,
+            {
                 "type": "signaling_message",
                 "message": {
                     "type": "join",
                     "userId": str(self.user.id),
-                }
-            }
+                },
+            },
         )
-        
+
         response = await self.communicator.receive_json_from(timeout=5)
-        
-        self.assertEqual(response, {"type": "join", "userId": str(self.user.id)}, "Got an invalid JSON response")
-    
+
+        self.assertEqual(
+            response,
+            {"type": "join", "userId": str(self.user.id)},
+            "Got an invalid JSON response",
+        )
+
         await self.tearDown()
-    
+
     async def test_private_data_signaling(self):
         await self.asyncSetUp()
-        
+
         channel_layer = get_channel_layer()
         group = f"private_user{self.user.id}"
-        
+
         data = {
             "type": "signaling_message",
             "message": {
                 "type": "test_signal",
                 "to": str(self.user.id),
                 "from": str(self.user.id),
-            }
+            },
         }
-        
-        await channel_layer.group_send(
-            group, data
-        )
-        
+
+        await channel_layer.group_send(group, data)
+
         response = await self.communicator.receive_json_from(timeout=5)
-        
-        self.assertEqual(response, {
-            "type": "test_signal",
-            "to": str(self.user.id),
-            "from": str(self.user.id),
-        }, "Got invalid JSON response.")
-        
+
+        self.assertEqual(
+            response,
+            {
+                "type": "test_signal",
+                "to": str(self.user.id),
+                "from": str(self.user.id),
+            },
+            "Got invalid JSON response.",
+        )
+
         await self.tearDown()
+
 
 class MessageHistoryTest(TestCase):
     def setUp(self):
         self.mainURL = "/conference/api/get-message-history/"
-        
+
         User = get_user_model()
         self.user = User.objects.create_testuser()
-        
+
         self.client = Client()
-        
+
         logged_in = self.client.login(username="test", password="test")
         self.assertTrue(logged_in, "Failed to log into testing account.")
-        
+
         self.conference = Conference.objects.create(host=self.user)
         self.conference.save()
-    
+
     def test_unauthorized(self):
         self.client.logout()
         request = self.client.get(f"{self.mainURL}?token=something")
-        
+
         self.assertIsNotNone(request.headers, "Got an empty response.")
-        self.assertEqual(request.status_code, 403, "You shouldn't be able to access this API unauthorized.")
-    
+        self.assertEqual(
+            request.status_code,
+            403,
+            "You shouldn't be able to access this API unauthorized.",
+        )
+
     def test_request(self):
         request = self.client.get(f"{self.mainURL}?token={self.conference.token}")
-        
+
         self.assertIsNotNone(request.headers, "Got an empty response.")
         self.assertEqual(request.status_code, 200, "Got an invalid status code.")
-        
+
         json = request.json()
-        
+
         self.assertIsNotNone(json, "Got an empty JSON response.")
-        
+
         self.assertTrue(json.get("success"), "JSON success is False.")
         self.assertIsNotNone(json.get("history"), "Missing history field.")
-        
+
     def test_request_invalid_token(self):
         request = self.client.get(f"{self.mainURL}?token=some-invalid-token")
-        
+
         self.assertIsNotNone(request.headers, "Got an empty response.")
         self.assertEqual(request.status_code, 400, "Invalid status code given.")
-        
+
     def test_request_no_token(self):
         request = self.client.get(f"{self.mainURL}")
-        
+
         self.assertIsNotNone(request.headers, "Got an empty response.")
         self.assertEqual(request.status_code, 400, "Invalid status code given.")
+
+
+class GetHostIDTest(TestCase):
+    def setUp(self):
+        self.mainURL = "/conference/api/get-host/"
+
+        User = get_user_model()
+        self.user = User.objects.create_testuser()
+
+        self.client = Client()
+
+        logged_in = self.client.login(username="test", password="test")
+        self.assertTrue(logged_in, "Failed to log into testing account.")
+
+        self.conference = Conference.objects.create(host=self.user)
+        self.conference.save()
+
+    def test_unauthorized(self):
+        self.client.logout()
+        request = self.client.get(f"{self.mainURL}?token=something")
+
+        self.assertIsNotNone(request.headers, "Got an empty response.")
+        self.assertEqual(
+            request.status_code,
+            403,
+            "You shouldn't be able to access this API unauthorized.",
+        )
+
+    def test_request(self):
+        request = self.client.get(f"{self.mainURL}?token={self.conference.token}")
+
+        self.assertIsNotNone(request.headers, "Got an empty response.")
+        self.assertEqual(
+            request.status_code, 200, "Request returned an invalid response code."
+        )
+
+        json = request.json()
+
+        self.assertIsNotNone(json, "JSON response missing.")
+        self.assertTrue(json.get("success"), "JSON success is False.")
+        self.assertEqual(
+            json.get("host"), self.conference.host.id, "Invalid host ID given."
+        )
+
+    def test_invalid(self):
+        request = self.client.get(f"{self.mainURL}?token=totally_invalid")
+
+        self.assertIsNotNone(request.headers, "Got an empty response.")
+        self.assertEqual(
+            request.status_code, 400, "Request returned an invalid response code."
+        )
+
+    def test_missing(self):
+        request = self.client.get(f"{self.mainURL}")
+
+        self.assertIsNotNone(request.headers, "Got an empty response.")
+        self.assertEqual(
+            request.status_code, 400, "Request returned an invalid response code."
+        )
