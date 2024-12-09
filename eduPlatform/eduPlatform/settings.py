78b854680102d32bd5e1fa8a52d1f4ac1cb399a6
finista <95535357+finista@users.noreply.github.com>
2024-12-09 21:22:26 +0300

    Added meeting duration
    Added max meeting duration upon expiration of which, the conference will be ended automatically. Set up celery for this task.

--- eduPlatform/eduPlatform/settings.py
+++ eduPlatform/eduPlatform/settings.py
@@ -141,12 +141,18 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [os.getenv('REDIS_URL', 'redis://localhost:6379')],
+            'hosts': [os.getenv('REDIS_URL', 'redis://localhost:6379/0')],
         },
-    } if IS_PRODUCTION else {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     }
 }
+
+# Celery settings
+
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
