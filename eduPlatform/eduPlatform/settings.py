943a85c8849e23cbeb2f6fd4dac08b9e9ad79519
finista <95535357+finista@users.noreply.github.com>
2024-12-09 22:03:20 +0300

    Misc

--- eduPlatform/eduPlatform/settings.py
+++ eduPlatform/eduPlatform/settings.py
@@ -141,7 +141,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [os.getenv('REDIS_URL', 'redis://localhost:6379/0')],
+            'hosts': [os.getenv('REDIS_URL', 'redis://redis:6379/0')],
         },
     }
 }
@@ -149,7 +149,7 @@ CHANNEL_LAYERS = {
 # Celery settings
 
 CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://redis:6379/0')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
