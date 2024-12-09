ddb7f2abdfc2f83b82d0a06e3b7b201012b11595
finista <95535357+finista@users.noreply.github.com>
2024-12-09 22:17:47 +0300

    Fix settings.py

--- eduPlatform/eduPlatform/settings.py
+++ eduPlatform/eduPlatform/settings.py
@@ -143,12 +143,14 @@ CHANNEL_LAYERS = {
         'CONFIG': {
             'hosts': [os.getenv('REDIS_URL', 'redis://redis:6379/0')],
         },
+    } if IS_PRODUCTION else {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     }
 }
 
 # Celery settings
 
-CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://redis:6379/0')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
